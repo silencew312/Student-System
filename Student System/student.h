65a8d5426d00de65337787091dd6a0632343814d
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:48 +0800

    Create student.h

---
+++ Student System/student.h
@@ -0,0 +1,21 @@
+﻿#ifndef STUDENT_H
+#define STUDENT_H
+#include "subject.h"
+#include<QList>
+#include <QString>
+#include"cclass.h"
+class Student
+{
+public:
+      long id;
+      QString name;
+       QString sex;
+       QString phone;
+       QString password;
+       int classId;
+       QList<double>score;
+       Student();
+      Student(long Id,QString Name,QString Sex,QString Phone,int ClassId);
+};
+
+#endif // STUDENT_H
