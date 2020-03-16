4b914a7f8bc5bf1e1bc12fffed847ad99336d3c9
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:46 +0800

    Create student.cpp

---
+++ Student System/student.cpp
@@ -0,0 +1,16 @@
+﻿#include "student.h"
+
+Student::Student()
+{
+
+}
+Student::Student(long Id,QString Name,QString Sex,QString P,int ClassId)
+{
+id=Id;
+name=Name;
+sex=Sex;
+phone=P;
+password="123456";
+classId=ClassId;
+}
+
