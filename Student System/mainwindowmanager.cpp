e54de764ddc7e7bbc21843d52f6e0d2441ba0010
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:06:57 +0800

    Create mainwindowmanager.cpp

---
+++ Student System/mainwindowmanager.cpp
@@ -0,0 +1,36 @@
+﻿#include "mainwindowmanager.h"
+#include "ui_mainwindowmanager.h"
+
+
+MainWindowManager::MainWindowManager(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindowManager)
+{
+    ui->setupUi(this);
+}
+
+MainWindowManager::~MainWindowManager()
+{
+
+        delete ui;
+    if(LC.size()!=0){
+    Handle::Writef(LC);
+
+    for(int j=0;j<LC.size();j++)
+        Handle::Writef(LC[j],LStu[j]);}
+
+}
+
+
+void MainWindowManager::on_baseBtn_clicked()
+{
+    Base*b=new Base(this);
+    b->show();
+}
+
+void MainWindowManager::on_gBtn_clicked()
+{  mgrade*m=new mgrade(this);
+    m->show();
+
+}
+
