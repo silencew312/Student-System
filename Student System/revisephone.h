7a7be675a997738421afaab534f7f1e87a7c6c68
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:29 +0800

    Create revisephone.h

---
+++ Student System/revisephone.h
@@ -0,0 +1,31 @@
+﻿#ifndef REVISEPHONE_H
+#define REVISEPHONE_H
+#include<QString>
+#include <QDialog>
+#include"student.h"
+#include<QMessageBox>
+extern Student SS;
+extern QList<Student>LSStu;
+
+extern int A;
+
+namespace Ui {
+class revisePhone;
+}
+
+class revisePhone : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit revisePhone(QWidget *parent = 0);
+    ~revisePhone();
+
+private slots:
+    void on_rpBtn_ok_clicked();
+
+private:
+    Ui::revisePhone *ui;
+};
+
+#endif // REVISEPHONE_H
