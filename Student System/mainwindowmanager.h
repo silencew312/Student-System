3344d822e3cf4b70d2619dec51764848ea4a1948
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:06:59 +0800

    Create mainwindowmanager.h

---
+++ Student System/mainwindowmanager.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAINWINDOWMANAGER_H
+#define MAINWINDOWMANAGER_H
+#include "mgrade.h"
+#include <QMainWindow>
+#include "base.h"
+#include"handle.h"
+#include<QString>
+extern QList<QList<Student>>LStu;
+extern QList<CClass>LC;
+namespace Ui {
+class MainWindowManager;
+}
+
+class MainWindowManager : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindowManager(QWidget *parent = 0);
+    ~MainWindowManager();
+
+
+
+private slots:
+    void on_baseBtn_clicked();
+
+    void on_gBtn_clicked();
+
+
+
+private:
+    Ui::MainWindowManager *ui;
+};
+
+#endif // MAINWINDOW3_H
