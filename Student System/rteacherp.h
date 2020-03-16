da45ee7421f1cb6906a975e33612aad6ef205169
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:42 +0800

    Create rteacherp.h

---
+++ Student System/rteacherp.h
@@ -0,0 +1,30 @@
+﻿#ifndef RTEACHERP_H
+#define RTEACHERP_H
+#include "student.h"
+#include"cclass.h"
+#include <QDialog>
+#include<QMessageBox>
+#include<QString>
+extern QList<QList<Student>>LStu;
+extern QList<int> B;
+extern QList<CClass>LC;
+namespace Ui {
+class rteacherP;
+}
+
+class rteacherP : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit rteacherP(QWidget *parent = 0);
+    ~rteacherP();
+
+private slots:
+    void on_RBtn_ok_clicked();
+
+private:
+    Ui::rteacherP *ui;
+};
+
+#endif // RTEACHERP_H
