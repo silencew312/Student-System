f6e0c1fa3336a10dc84616667e798689d7c2fceb
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:36 +0800

    Create rstup.h

---
+++ Student System/rstup.h
@@ -0,0 +1,30 @@
+﻿#ifndef RSTUP_H
+#define RSTUP_H
+#include"student.h"
+#include <QDialog>
+#include<QMessageBox>
+#include<QString>
+extern Student SS;
+extern QList<Student>LSStu;
+
+extern int A;
+namespace Ui {
+class rstuP;
+}
+
+class rstuP : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit rstuP(QWidget *parent = 0);
+    ~rstuP();
+
+private slots:
+    void on_RBtn_ok_clicked();
+
+private:
+    Ui::rstuP *ui;
+};
+
+#endif // RSTUP_H
