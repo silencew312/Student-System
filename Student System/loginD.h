02cdaf92df7fdd319652f08f813303e88b730293
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:06:51 +0800

    Create loginD.h

---
+++ Student System/loginD.h
@@ -0,0 +1,36 @@
+﻿#ifndef LOGIND_H
+#define LOGIND_H
+#include <QDialog>
+#include "handle.h"
+#include<QMessageBox>
+#include "cclass.h"
+#include<QString>
+extern QList<QList<Student>>LStu;
+extern Student SS;
+extern int A;
+extern QList<int> B;
+extern QList<CClass>LC;
+extern QList<Student>LSStu;
+namespace Ui {
+class LoginD;
+}
+
+class LoginD : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginD(QWidget *parent = 0);
+    ~LoginD();
+
+private slots:
+    void on_loginBtn_clicked();
+public:
+    int CM;
+
+
+private:
+    Ui::LoginD *ui;
+};
+
+#endif // LOGIND_H
