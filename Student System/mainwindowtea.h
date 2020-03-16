fe296357ba4db7a92dd49d1f8ae3f60970435c63
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:12 +0800

    Create mainwindowtea.h

---
+++ Student System/mainwindowtea.h
@@ -0,0 +1,54 @@
+﻿#ifndef MAINWINDOWTEA_H
+#define MAINWINDOWTEA_H
+#include "rteacherp.h"
+#include <QMainWindow>
+#include"handle.h"
+#include<QString>
+extern QList<int> B;
+extern QList<QList<Student>>LStu;
+extern QList<CClass>LC;
+namespace Ui {
+class MainWindowTea;
+}
+
+class MainWindowTea : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindowTea(QWidget *parent = 0);
+    ~MainWindowTea();
+
+void query(long key,QString str,int n,QList<Student>Stu);
+
+void NewBoxInformation(int i);
+private slots:
+    void on_gqBtn_clicked();
+
+    void on_anBtn_clicked();
+
+    void on_hlBtn_clicked();
+
+    void on_tabWidget_currentChanged(int index);
+
+    void on_trBtn_clicked();
+
+
+
+
+
+    void on_outgradeBtn_clicked();
+
+    void on_sortBtn_clicked();
+
+    void on_agBtn_clicked();
+
+    void on_dgBtn_clicked();
+
+    void on_qBtn_class_clicked();
+
+private:
+    Ui::MainWindowTea *ui;
+};
+
+#endif // MAINWINDOW2_H
