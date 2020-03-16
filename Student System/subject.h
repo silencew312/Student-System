ccb8c0c315f006ed19531bf3caf0d021736f628a
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:07:57 +0800

    Create subject.h

---
+++ Student System/subject.h
@@ -0,0 +1,20 @@
+﻿#ifndef SUBJECT_H
+#define SUBJECT_H
+#include<Qstring>
+#include"teacher.h"
+
+class Subject
+{
+public:
+    QString subname;
+
+
+    double credit;//学分
+
+    Subject();
+    Teacher teacher;
+    Subject(QString SubName,double Credit,Teacher T);
+
+};
+
+#endif // SUBJECT_H
